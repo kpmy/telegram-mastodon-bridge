d38569cfa583d9076db1b04b51d02d7bd4475669
kpmy <kpmy@yandex.ru>
2024-11-15 23:49:43 +0000

    add quoted chat name

--- bridge/helper.py
+++ bridge/helper.py
@@ -1,7 +1,6 @@
 from typing import List
 import telebot
 
-
 class Footer():
 
     def __forwarded_from(self, title: str) -> str:
@@ -11,6 +10,8 @@ class Footer():
     def __posted_in(self, name: str) -> str:
         return f"\r\rPosted in {name}"
 
+    def __quoted_of(self, title: str) -> str:
+        return f"\nQuoted of {title}"
 
     def __make_text(self, final_text: str, message: telebot.types.Message, character_limit: int) -> List[str]:
         if message.chat.username:
@@ -20,6 +21,9 @@ class Footer():
 
         if message.forward_from_chat:
             final_text += self.__forwarded_from(message.forward_from_chat.title)
+        
+        if message.quote:
+            final_text += self.__quoted_of(message.external_reply.chat.title)
 
         if len(final_text) <= character_limit:
             return [final_text]
